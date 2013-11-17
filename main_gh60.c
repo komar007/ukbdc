bf4b0283d0fd8216e0979978df514235245ee80e
Michal Trybus <komar007@gmail.com>
2013-11-17 23:25:25 +0100

    Support for split backspace

--- main_gh60.c
+++ main_gh60.c
@@ -22,7 +22,7 @@ uint8_t matrix[5][14] = {
 	{14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27},
 	{28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41},
 	{42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55},
-	{56, 57, 58, 0, 0, 59, 0, 0, 0, 0, 60, 61, 62, 63},
+	{56, 57, 58, 0, 0, 59, 0, 0, 0, 64, 60, 61, 62, 63},
 };
 
 uint8_t rows[] = {0, 1, 2, 3, 4};
@@ -44,8 +44,8 @@ int main(void)
 
 	USB_init();
 
-	/* initialize with 64 keys */
-	LAYOUT_init(64);
+	/* initialize with 65 keys */
+	LAYOUT_init(65);
 
 	LAYOUT_set((struct layout*)LAYOUT_BEGIN);
 	LAYOUT_set_callback(&HID_set_scancode_state);
