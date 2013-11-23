571703fde20b1e193a42386aa2a09c3f555209db
Michal Trybus <komar007@gmail.com>
2013-11-23 18:13:10 +0100

    added licensing information

--- usb_hardware.h
+++ usb_hardware.h
@@ -1,3 +1,20 @@
+/* This file is part of ukbdc.
+ *
+ * ukbdc is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * ukbdc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ukbdc; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 /* This file contains functions which manipulate the AVR's USB controller by
  * writing to SFRs and reading SFRs. It also contains definitions of
  * configuration parameters which are specific to AVR's USB controller and not
