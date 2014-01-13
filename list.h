7df85c04c2a11cf8226c04fbfe57fb2cb33ca96c
Michal Trybus <komar007@gmail.com>
2014-01-14 00:54:24 +0100

    [list] added doxygen

--- list.h
+++ list.h
@@ -15,6 +15,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/*! \file list.h
+ * A generic implementation of a singly-linked list */
+
 #pragma once
 
 #include <stdint.h>
@@ -25,12 +28,22 @@ typedef struct list_node {
 	void *data;
 } node_t;
 
+/*! The type representing a singly-linked list */
 typedef struct {
 	node_t *head;
 	node_t *tail;
 	size_t size;
 } list_t;
 
+/*! Initializes a list. This function must be called before any other
+ * functions on this list */
 void list_init(list_t *list);
+/*! Adds an element to the list
+ * \param list the list to add the element to
+ * \param data the element to add to the list
+ */
 void list_add(list_t *list, void *data);
+/*! Removes all elements from the list
+ * \param list the list to clear
+ */
 void list_clear(list_t *list);
