9e14871de6f3c41fbeba15f9546102cc857dc312
Michal Trybus <komar007@gmail.com>
2014-01-14 00:54:01 +0100

    [leds] added doxygen

--- leds.h
+++ leds.h
@@ -15,6 +15,15 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/*! \file leds.h
+ * This module provides a simple interface to control LEDs with soft
+ * transitions using software PWM.
+ *
+ * It also adds one layer of abstraction by assigning all LEDs numbers.
+ *
+ * This module uses TIMER0 exclusively!
+ */
+
 #pragma once
 
 #include <stdint.h>
@@ -30,16 +39,33 @@
 #define COMPOSE_MASK	0x08
 #define KANA_MASK	0x10
 
+/*! A structure defining one LED's state */
 struct led {
+	/*! Pin number (as defined in module IO (io.h)) */
 	int8_t pin;
+	/*! Current brightness level */
 	int8_t level;
+	/*! Action currently performed on the LED */
 	uint8_t action;
+	/*! Current binary state */
 	bool state;
 };
 
+/*! Initializes LED module. This function should be called before any other
+ * function in this module */
 void LED_init();
+/*! Sets the state of an LED
+ * \param led LED number
+ * \param state new binary state to set
+ */
 void LED_set(uint8_t led, bool state);
-void LED_handle_sof();
 void LED_set_indicators(uint8_t hid_leds);
+/*! Tests if an LED not in the process of changing state
+ * \param led LED number
+ * \return `true` if the LED is stable, `false` otherwise
+ */
 bool LED_stable(uint8_t led);
+/*! Tests if all LEDs are stable
+ * \return `true` if all LEDs are stable, `false` otherwise
+ */
 bool LED_all_stable();
