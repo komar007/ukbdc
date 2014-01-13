026e41ff9eed952173d52c5f5e4b14858e747cb0
Michal Trybus <komar007@gmail.com>
2014-01-14 00:12:59 +0100

    [io,aux] added doxygen

--- aux.h
+++ aux.h
@@ -15,16 +15,27 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/*! \file aux.h
+ * Auxilliary definitions and macros which don't fit anywhere else
+ */
+
 #pragma once
 
 #include <stddef.h>
 
+/*! Computes array size */
 #define ARR_SZ(a) (sizeof(a) / sizeof(*(a)))
+/*! Extracts the Less Significant Byte of a word */
 #define LSB(n) (n & 0xff)
+/*! Extracts the More Significant Byte of a word */
 #define MSB(n) ((n >> 8) & 0xff)
 
-/* Macro used to read a struct field from program memory (supported field
- * sizes: 1, 2 and 4 bytes) */
+/*! A macro used to read a struct field from program memory (supported field
+ * sizes: 1, 2 and 4 bytes)
+ * \param addr the address to the structure in flash
+ * \param field the name of the structure's field to be read
+ * \return the value of the field
+ */
 #define get_pgm_struct_field(addr, field) ( \
 	(sizeof(((typeof(*(addr))*)0)->field) == 1) ? \
 		pgm_read_byte((uint8_t*)(addr) + offsetof(typeof(*(addr)), field)) \
--- io.h
+++ io.h
@@ -15,6 +15,15 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+/*! \file io.h
+ * This module creates a layer of abstraction for microcontroller pins
+ * Every pin is represented by a 1-byte unsigned number. Numbers up to 127
+ * represent *internal* pins, which means pins belonging to microcontroller's
+ * ports. Numbers higher than that represent *external* pins. The external
+ * pins can be anything and the implementation of the writing, reading and
+ * configuring functions can be provided by the user.
+ */
+
 #pragma once
 
 #include "platforms.h"
@@ -25,9 +34,23 @@
 #include <avr/io.h>
 #include <stdbool.h>
 
+/*! A macro for pin's output mode */
 #define OUTPUT	true
+/*! A macro for pin's input mode */
 #define INPUT	false
 
+/*! Reads the state of pin
+ * \param pin pin number
+ * \return binary state of the pin
+ */
 bool IO_get(uint8_t pin);
+/*! Sets the state of pin
+ * \param pin pin number
+ * \oaran val the value to set
+ */
 void IO_set(uint8_t pin, bool val);
+/*! Sets the mode of a pin
+ * \param pin pin number
+ * \param dir the mode of the pin (\ref INPUT or \ref OUTPUT)
+ */
 void IO_config(uint8_t pin, bool dir);
